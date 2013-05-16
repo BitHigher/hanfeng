3d933f2ab57748060f8a10b1f271fbcb14a620f6
Zhu Hanfeng <zhuhanfeng@gmail.com>
2013-05-16 11:44:22 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zhf
+ *
+ * Created on 2013年5月16日, 上午11:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+

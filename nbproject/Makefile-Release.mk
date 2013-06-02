c6f12dd15b71a6628720fda211d01e35e59813f8
Zhu Hanfeng <zhuhanfeng@gmail.com>
2013-06-02 23:51:08 +0800

    re construct project

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -35,15 +35,33 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/base/HFObject.o \
+	${OBJECTDIR}/base/Parallel.o \
 	${OBJECTDIR}/base/init.o \
 	${OBJECTDIR}/base/memory.o \
+	${OBJECTDIR}/features/DotFeatures.o \
+	${OBJECTDIR}/features/Features.o \
+	${OBJECTDIR}/io/File.o \
 	${OBJECTDIR}/io/HFIO.o \
+	${OBJECTDIR}/labels/BinaryLabels.o \
+	${OBJECTDIR}/labels/DenseLabels.o \
+	${OBJECTDIR}/labels/Labels.o \
+	${OBJECTDIR}/labels/LatentLabels.o \
+	${OBJECTDIR}/labels/MultiClassLabels.o \
+	${OBJECTDIR}/labels/RegressionLabels.o \
+	${OBJECTDIR}/labels/StructuredLabels.o \
 	${OBJECTDIR}/lib/HFException.o \
+	${OBJECTDIR}/loss/LogLoss.o \
+	${OBJECTDIR}/machine/LinearMachine.o \
+	${OBJECTDIR}/machine/Machine.o \
 	${OBJECTDIR}/main.o \
 	${OBJECTDIR}/math/Math.o \
+	${OBJECTDIR}/math/lapack.o \
 	${OBJECTDIR}/structure/HFMatrix.o \
 	${OBJECTDIR}/structure/HFReferenceData.o \
-	${OBJECTDIR}/structure/HFVector.o
+	${OBJECTDIR}/structure/HFVector.o \
+	${OBJECTDIR}/structure/Subset.o \
+	${OBJECTDIR}/structure/SubsetStack.o
 
 
 # C Compiler Flags
@@ -70,6 +88,16 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/hanfeng: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/hanfeng ${OBJECTFILES} ${LDLIBSOPTIONS}
 
+${OBJECTDIR}/base/HFObject.o: base/HFObject.cpp 
+	${MKDIR} -p ${OBJECTDIR}/base
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/base/HFObject.o base/HFObject.cpp
+
+${OBJECTDIR}/base/Parallel.o: base/Parallel.cpp 
+	${MKDIR} -p ${OBJECTDIR}/base
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/base/Parallel.o base/Parallel.cpp
+
 ${OBJECTDIR}/base/init.o: base/init.cpp 
 	${MKDIR} -p ${OBJECTDIR}/base
 	${RM} $@.d
@@ -80,15 +108,80 @@ ${OBJECTDIR}/base/memory.o: base/memory.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/base/memory.o base/memory.cpp
 
+${OBJECTDIR}/features/DotFeatures.o: features/DotFeatures.cpp 
+	${MKDIR} -p ${OBJECTDIR}/features
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/features/DotFeatures.o features/DotFeatures.cpp
+
+${OBJECTDIR}/features/Features.o: features/Features.cpp 
+	${MKDIR} -p ${OBJECTDIR}/features
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/features/Features.o features/Features.cpp
+
+${OBJECTDIR}/io/File.o: io/File.cpp 
+	${MKDIR} -p ${OBJECTDIR}/io
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/io/File.o io/File.cpp
+
 ${OBJECTDIR}/io/HFIO.o: io/HFIO.cpp 
 	${MKDIR} -p ${OBJECTDIR}/io
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/io/HFIO.o io/HFIO.cpp
 
+${OBJECTDIR}/labels/BinaryLabels.o: labels/BinaryLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/BinaryLabels.o labels/BinaryLabels.cpp
+
+${OBJECTDIR}/labels/DenseLabels.o: labels/DenseLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/DenseLabels.o labels/DenseLabels.cpp
+
+${OBJECTDIR}/labels/Labels.o: labels/Labels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/Labels.o labels/Labels.cpp
+
+${OBJECTDIR}/labels/LatentLabels.o: labels/LatentLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/LatentLabels.o labels/LatentLabels.cpp
+
+${OBJECTDIR}/labels/MultiClassLabels.o: labels/MultiClassLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/MultiClassLabels.o labels/MultiClassLabels.cpp
+
+${OBJECTDIR}/labels/RegressionLabels.o: labels/RegressionLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/RegressionLabels.o labels/RegressionLabels.cpp
+
+${OBJECTDIR}/labels/StructuredLabels.o: labels/StructuredLabels.cpp 
+	${MKDIR} -p ${OBJECTDIR}/labels
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/labels/StructuredLabels.o labels/StructuredLabels.cpp
+
 ${OBJECTDIR}/lib/HFException.o: lib/HFException.cpp 
 	${MKDIR} -p ${OBJECTDIR}/lib
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/lib/HFException.o lib/HFException.cpp
+
+${OBJECTDIR}/loss/LogLoss.o: loss/LogLoss.cpp 
+	${MKDIR} -p ${OBJECTDIR}/loss
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/loss/LogLoss.o loss/LogLoss.cpp
+
+${OBJECTDIR}/machine/LinearMachine.o: machine/LinearMachine.cpp 
+	${MKDIR} -p ${OBJECTDIR}/machine
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/machine/LinearMachine.o machine/LinearMachine.cpp
+
+${OBJECTDIR}/machine/Machine.o: machine/Machine.cpp 
+	${MKDIR} -p ${OBJECTDIR}/machine
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/machine/Machine.o machine/Machine.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
@@ -99,6 +192,11 @@ ${OBJECTDIR}/math/Math.o: math/Math.cpp
 	${MKDIR} -p ${OBJECTDIR}/math
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/math/Math.o math/Math.cpp
+
+${OBJECTDIR}/math/lapack.o: math/lapack.cpp 
+	${MKDIR} -p ${OBJECTDIR}/math
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/math/lapack.o math/lapack.cpp
 
 ${OBJECTDIR}/structure/HFMatrix.o: structure/HFMatrix.cpp 
 	${MKDIR} -p ${OBJECTDIR}/structure
@@ -114,6 +212,16 @@ ${OBJECTDIR}/structure/HFVector.o: structure/HFVector.cpp
 	${MKDIR} -p ${OBJECTDIR}/structure
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/structure/HFVector.o structure/HFVector.cpp
+
+${OBJECTDIR}/structure/Subset.o: structure/Subset.cpp 
+	${MKDIR} -p ${OBJECTDIR}/structure
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/structure/Subset.o structure/Subset.cpp
+
+${OBJECTDIR}/structure/SubsetStack.o: structure/SubsetStack.cpp 
+	${MKDIR} -p ${OBJECTDIR}/structure
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/structure/SubsetStack.o structure/SubsetStack.cpp
 
 # Subprojects
 .build-subprojects:

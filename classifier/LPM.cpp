55b2fa49664beb937fd3cfeb693be42672b8ac0d
Zhu Hanfeng <zhuhanfeng@gmail.com>
2013-06-03 12:13:15 +0800

    bug fix

--- classifier/LPM.cpp
+++ classifier/LPM.cpp
@@ -5,14 +5,24 @@
  * Created on 2013年6月3日, 上午10:26
  */
 
+#include "../lib/config.h"
+
+#ifdef USE_CPLEX
+
 #include "LPM.h"
 
-CLPM::CLPM() {
+using namespace hanfeng;
+
+CLPM::CLPM()
+{
 }
 
-CLPM::CLPM(const CLPM &orig) {
+CLPM::CLPM(const CLPM &orig)
+{
 }
 
-CLPM::~CLPM() {
+CLPM::~CLPM()
+{
 }
 
+#endif
